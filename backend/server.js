8c0f28215b57868175960a680a864afe022b7735
Amaliya Ibragimova <i.amaliya.f@gmail.com>
2025-12-17 10:33:59 +0300

    lab 10-11 prototype1 - возвращение к рабочей версии

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 import Fastify from 'fastify';
 import FastifyStatic from '@fastify/static';
-import fastifyPostgres from '@fastify/postgres';
-import fastifyJwt from '@fastify/jwt';
+import fastifyPostgres from '@fastify/postgres'; // <--- Импорт
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-
+// __dirname теперь будет /app/backend/ (внутри контейнера)
 const __dirname = path.dirname(__filename);
 
 const fastify = Fastify({ logger: true });
@@ -17,22 +16,6 @@ dotenv.config({ path: '.env.local' });
 fastify.register(fastifyPostgres, {
   connectionString: process.env.DATABASE_URL
 });  
-
-// if (!process.env.SECRET_KEY) {
-//   throw new Error('SECRET_KEY не задан в .env.local');
-// }
-
-// fastify.register(fastifyJwt, {
-//   secret: process.env.SECRET_KEY
-// });
-
-// fastify.decorate("authenticate", async function (request, reply) {
-//   try {
-//     await request.jwtVerify();
-//   } catch (err) {
-//     reply.code(401).send({ error: "Unauthorized" });
-//   }
-// });
 
 // --- РЕГИСТРАЦИЯ СТАТИКИ ---
 
@@ -48,8 +31,6 @@ import lab9Routes from './src/routes/lab9/index.js';
 import task2Routes from './src/routes/lab9/task2.js';
 import task3Routes from './src/routes/lab9/task3.js';
 
-import lab10Routes from './src/routes/lab10/index.js';
-import lab11Routes from './src/routes/lab11/index.js';
 
 fastify.register(lab7Routes, { prefix: '/api/lab7' });
 fastify.register(lab8Routes, { prefix: '/api/lab8' });
@@ -57,15 +38,7 @@ fastify.register(lab9Routes, { prefix: '/api/lab9' });
 fastify.register(task2Routes, { prefix: '/api/lab9/task2' });
 fastify.register(task3Routes, { prefix: '/api/lab9/task3' });
 
-// fastify.register(lab10Routes, {
-//   prefix: '/api/lab10',
-//   authenticate: fastify.authenticate
-// });
-
-// fastify.register(lab11Routes, {
-//   prefix: '/api/lab11',
-//   authenticate: fastify.authenticate
-// });
+// Здесь добавляем новые строки для каждой лабы
 
 // --- ЗАПУСК СЕРВERA ---
 const start = async () => {
